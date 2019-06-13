3a9315562f89cdfb3485ac63935cc296e529c4f4
leo <lonely.leo.leo@gmail.com>
2019-06-13 22:37:23 +0300

    bot code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ cities = {
 }
 
 
-def get_weather(lat, lon):
+def get_forecast(lat, lon):
     conn = h.HTTPSConnection("api.weather.yandex.ru")
     conn.request("GET", "/v1/forecast?lat=" + lat + "&lon=" + lon + "&limit=1",
                  headers={
@@ -53,7 +53,19 @@ def parse_answer(weather_answer):
         res_str += "\nWind speed: " + str(cws["wind_speed"])
         res_str += "\nWind direction " + str(cws["wind_dir"])
         res_str += "\n" + str(cws["condition"])
+    return res_str
 
+
+def parse_answer_today(weather_answer):
+    tom_d = weather_answer["now"]
+    cws = 0
+    res_str = ""
+    cws = weather_answer["fact"]
+    if cws != 0:
+        res_str += "Temperature: " + str(cws["temp"])
+        res_str += "\nWind speed: " + str(cws["wind_speed"])
+        res_str += "\nWind direction " + str(cws["wind_dir"])
+        res_str += "\n" + str(cws["condition"])
     return res_str
 
 
@@ -66,26 +78,43 @@ bot = telebot.TeleBot("894480080:AAH5CGQulI-y00ldJxJN7YPDsizRmszw_Jg")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello there ladies and gentleman!")
+    bot.reply_to(message, "Hello there ladies and gentleman! "
+                          "Allow me to introduce myself. "
+                          "My name is weatherBot and i provide weather info. "
+                          "Type '/get_weather *town_name*' for current information on the area. "
+                          "Type '/get_forecast *town_name*' to find out tomorrow average temperature and weather.")
 
 
-@bot.message_handler(commands=['get_weather'])
+@bot.message_handler(commands=['get_forecast'])
 def send_tomorrow_weather(message):
     text = message.text.split()[1]
     if text not in cities.keys():
         bot.reply_to(message, "invalid city")
         return
     coords = cities[text]
-    result = get_weather(coords[0], coords[1])
+    result = get_forecast(coords[0], coords[1])
     result_str = parse_answer(result)
     bot.reply_to(message, result_str)
 
 
+@bot.message_handler(commands=['get_weather'])
+def send_today_weather(message):
+    text = message.text.split()[1]
+    if text not in cities.keys():
+        bot.reply_to(message, "invalid city")
+        return
+    coords = cities[text]
+    result = get_forecast(coords[0], coords[1])
+    result_str = parse_answer_today(result)
+    bot.reply_to(message, result_str)
+
+
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "Sorry? Try to type '/get_weather *Town_name*'.")
+    bot.reply_to(message, "Sorry? Try to type '/get_forecast *Town_name*'.")
 
 
-# res = get_weather("55.75396", "37.620393")
+# res = get_forecast("55.75396", "37.620393")
 bot.polling()
 # print(get_tomorrow_date("2016-08-03"))
